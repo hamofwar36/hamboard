bf79dedfbdf09e01a0d89b91433091dd02f34766
hamofwar36 <47081180+hamofwar36@users.noreply.github.com>
2026-09-18 19:13:20 +0900

    Delete mobile/src-tauri directory

--- mobile/src-tauri/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-env-changed=HAMBOARD_GOOGLE_OAUTH_CLIENT_ID");
-    println!("cargo:rerun-if-env-changed=HAMBOARD_GOOGLE_OAUTH_CLIENT_SECRET");
-    tauri_build::build()
-}
